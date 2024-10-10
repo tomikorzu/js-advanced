f0ce3684d76048a2af1245a6df71176eba610e4d
tomikorzu <tomykorzu@icloud.com>
2024-10-10 09:45:42 -0300

    feat(index.js): add the first excercise including functionVar function and other variables

---
+++ scope/index.js
@@ -0,0 +1,15 @@
+const globalVar = "I am a global variable";
+
+const functionVar = () => {
+  if (typeof globalVar === toString) {
+    const a = 5;
+    console.log("The global var type is string");
+  } else {
+    console.log(`The global vara type is ${typeof globalVar}`);
+  }
+  for (let a = 0; a < 10; a++) {
+    console.log(a);
+  }
+};
+
+functionVar();
