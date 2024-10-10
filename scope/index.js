2ec1e6b15e4e78058cc53eb82a56221407914597
tomikorzu <tomykorzu@icloud.com>
2024-10-10 09:48:43 -0300

    fix(index.js): change the blocksVariable names

--- scope/index.js
+++ scope/index.js
@@ -2,14 +2,16 @@ const globalVar = "I am a global variable";
 
 const functionVar = () => {
   if (typeof globalVar === toString) {
-    const a = 5;
+    const blockVar = 5;
     console.log("The global var type is string");
   } else {
     console.log(`The global vara type is ${typeof globalVar}`);
   }
-  for (let a = 0; a < 10; a++) {
-    console.log(a);
+  for (let blockVar = 0; blockVar < 10; blockVar++) {
+    console.log(blockVar);
   }
 };
 
 functionVar();
+
+// Closures
