2601169e5f43fc365015a0333dbbb4c0a873fd6b
tomikorzu <tomykorzu@icloud.com>
2024-10-14 10:26:48 -0300

    feat(index.js): add the first consign to create the base Vehicle prototype

--- prototypes/index.js
+++ prototypes/index.js
@@ -1,1 +1,39 @@
 console.log("Prototypes");
+console.log("Vehicle System Exercise");
+
+/*
+    @function vehicle
+    @returns {object} vehicle
+*/
+
+function Vehicle(mark, model, maxSpeed) {
+  this.mark = mark;
+  this.model = model;
+  this.maxSpeed = maxSpeed;
+}
+
+Vehicle.prototype.accelerate = function () {
+  console.log("Accelerating");
+};
+
+Vehicle.prototype.stop = function () {
+  console.log("Stopping");
+};
+
+Vehicle.prototype.showInfo = function () {
+  console.log(
+    `The mark is ${this.mark}, the model: ${this.model} and the max speed is: ${this.maxSpeed}`
+  );
+};
+
+const tiguan = new Vehicle("Volkswagen", "Tiguan", 220);
+console.log(tiguan);
+
+tiguan.accelerate();
+
+setTimeout(() => {
+  tiguan.stop();
+  setTimeout(() => {
+    tiguan.showInfo();
+  }, 1000);
+}, 1000);
