a4ee5100c534afdb26fd00b019672bc3d91b01a9
tomikorzu <tomykorzu@icloud.com>
2024-10-15 11:59:21 -0300

    feat(index.js): add buyTicket and buyTicketsForAllUsers functions

--- raceConditions/index.js
+++ raceConditions/index.js
@@ -13,6 +13,30 @@ console.log(`Total users: ${users.length}`);
 
 let lock = false;
 
+/*
+    This function simulates a buy of a ticket with a random delay
+*/
+const buyATicket = async (user) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (totalTicketsAvailable > 0 && !lock) {
+        lock = true;
+        totalTicketsAvailable -= 1;
+        console.log(`The user ${user} bought a ticket`);
+        console.log(`Total tickets available: ${totalTicketsAvailable}`);
+        lock = false;
+        resolve();
+      } else {
+        console.log(`The user ${user} couldn't buy a ticket`);
+        resolve();
+      }
+    }, Math.random() * 1000);
+  });
+};
 
-
-
+const buyTicketsForAllUsers = async () => {
+  users.forEach(async (user) => {
+    await buyATicket(user);
+  });
+};
+buyTicketsForAllUsers();
