b3d9bc768fe87e5a910aa25f7e2fe35434162f1f
tomikorzu <tomykorzu@icloud.com>
2024-10-16 10:21:16 -0300

    feat(index.js): change the download structure and add the promiseAny example

--- promises/index.js
+++ promises/index.js
@@ -1,3 +1,5 @@
+import kleur from "kleur";
+
 console.log("Promises");
 
 class fileDownload {
@@ -6,22 +8,39 @@ class fileDownload {
     this.fileSize = fileSize;
   }
   download() {
-    const time = this.fileSize * 100;
+    const randomTime = Math.floor(Math.random() * 100);
+    const fileSizeInMB = parseInt(this.fileSize);
+    const time = fileSizeInMB * randomTime;
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        resolve(
-          `The download of the file "${this.fileName}" was successful in ${
-            time / 1000
-          } seconds`
-        );
+        resolve({
+          name: this.fileName,
+          time: time,
+          message: `The download of the file "${
+            this.fileName
+          }" was successful in ${time / 1000} seconds`,
+        });
       }, time);
     });
   }
 }
 
-const windows = new fileDownload("Windows 11", 50);
-const ubuntu = new fileDownload("Ubuntu", 25);
+const windows = new fileDownload("Windows 11", "50mb");
+const ubuntu = new fileDownload("Ubuntu", "25mb");
 
-Promise.all([windows.download(), ubuntu.download()])
-  .then((response) => console.log(response))
+const downloads = [windows.download(), ubuntu.download()];
+
+Promise.all(downloads)
+  .then((response) => console.log(response.map((r) => {return r.message})))
+  .catch((error) => console.log("There was an error:", error));
+
+
+Promise.any(downloads)
+  .then((response) =>
+    console.log(
+      `The first file that was downloaded was ${kleur.green(
+        response.name
+      )}`
+    )
+  )
   .catch((error) => console.log("There was an error:", error));
