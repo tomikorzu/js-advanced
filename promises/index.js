2a269516683d3daf6f89de39897668e8e5db01f1
tomikorzu <tomykorzu@icloud.com>
2024-10-16 10:36:54 -0300

    feat(index.js): change the download function system

--- promises/index.js
+++ promises/index.js
@@ -3,44 +3,60 @@ import kleur from "kleur";
 console.log("Promises");
 
 class fileDownload {
-  constructor(fileName, fileSize) {
+  constructor(fileName, fileSize, result) {
     this.fileName = fileName;
-    this.fileSize = fileSize;
+    this.fileSize = parseInt(fileSize);
+    this.result = result;
   }
+
   download() {
     const randomTime = Math.floor(Math.random() * 100);
-    const fileSizeInMB = parseInt(this.fileSize);
-    const time = fileSizeInMB * randomTime;
+    const downloadTime = this.fileSize * randomTime;
+
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        resolve({
-          name: this.fileName,
-          time: time,
-          message: `The download of the file "${
-            this.fileName
-          }" was successful in ${time / 1000} seconds`,
-        });
-      }, time);
+        if (this.result) {
+          resolve({
+            name: this.fileName,
+            time: downloadTime,
+            message: `The download of the file "${
+              this.fileName
+            }" was successful in ${downloadTime / 1000} seconds`,
+          });
+        } else {
+          reject({
+            name: this.fileName,
+            time: downloadTime,
+            message: `The download of the file "${this.fileName}" failed in ${
+              downloadTime / 1000
+            } seconds`,
+          });
+        }
+      }, downloadTime);
     });
   }
 }
 
-const windows = new fileDownload("Windows 11", "50mb");
-const ubuntu = new fileDownload("Ubuntu", "25mb");
+const windows = new fileDownload("Windows 11", "50mb", false);
+const ubuntu = new fileDownload("Ubuntu", "25mb", true);
+const macOS = new fileDownload("macOS", "5mb", true);
 
-const downloads = [windows.download(), ubuntu.download()];
+const downloads = [windows.download(), ubuntu.download(), macOS.download()];
 
 Promise.all(downloads)
-  .then((response) => console.log(response.map((r) => {return r.message})))
+  .then((response) =>
+    console.log(
+      response.map((r) => {
+        return r.message;
+      })
+    )
+  )
   .catch((error) => console.log("There was an error:", error));
-
 
 Promise.any(downloads)
   .then((response) =>
     console.log(
-      `The first file that was downloaded was ${kleur.green(
-        response.name
-      )}`
+      `The first file that was downloaded was ${kleur.green(response.name)}`
     )
   )
   .catch((error) => console.log("There was an error:", error));
