529567a54731be9b31edd43830e5f3cec3d55b19
tomikorzu <tomykorzu@icloud.com>
2024-10-17 10:48:48 -0300

    feat(performance): add the cicles functions

--- performance/index.js
+++ performance/index.js
@@ -1,1 +1,65 @@
 console.log("Performance");
+
+const example1 = () => {
+  const t0 = performance.now();
+  doSomething();
+  const t1 = performance.now();
+  console.log(`Call to doSomething took ${t1 - t0} milliseconds.`);
+
+  function doSomething() {
+    for (let i = 0; i < 1000000; i++) {
+      console.log(i);
+    }
+  }
+};
+// example1();
+
+const example2 = () => {
+  console.time("doSomething");
+  doSomething();
+  console.timeEnd("doSomething");
+  function doSomething() {
+    for (let i = 0; i < 1000000; i++) {
+      console.log(i);
+    }
+  }
+};
+// example2();
+
+console.time("For Loop");
+
+// forCicle();
+// forEachCicle();
+// whileCicle();
+
+console.timeEnd("For Loop");
+
+function forCicle() {
+  for (let i = 0; i < 1000000; i++) {
+    console.log(i);
+  }
+}
+
+// ForClicle 17.431s
+
+function forEachCicle() {
+  let array = [];
+  for (let i = 0; i < 1000000; i++) {
+    array.push(i);
+  }
+  array.forEach((element) => {
+    console.log(element);
+  });
+}
+
+// ForEachCicle 17.901s
+
+function whileCicle() {
+  let i = 0;
+  while (i < 1000000) {
+    console.log(i);
+    i++;
+  }
+}
+
+// WhileCicle 17.749s
